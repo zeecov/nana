b29085ca793817ec9e8f7dfecfc93d51664f2e3d
Isaac Ogunmola <isaacogunmola@gmail.com>
2026-08-30 17:54:54 +0000

    This is the initial code

--- user_data.sh
+++ user_data.sh
@@ -2,4 +2,4 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install apache2 -y
 sudo systemctl restart apache2
-sudo echo "Hello world" >> /var/www/html/index.html
+sudo cp index.html /var/www/html/index.html
